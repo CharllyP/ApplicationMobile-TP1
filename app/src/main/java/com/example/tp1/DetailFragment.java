c0a4962971aa2b31f79ab939f89e26d6c38b8db4
Charlly PIERRE <charlly.pierre@alumni.univ-avignon.fr>
2021-02-19 16:40:56 +0100

    Initial commit

---
+++ app/src/main/java/com/example/tp1/DetailFragment.java
@@ -0,0 +1,42 @@
+package com.example.tp1;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+public class DetailFragment extends Fragment {
+
+    TextView titre;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        titre = view.findViewById(R.id.titreNomPays);
+
+        DetailFragmentArgs args = DetailFragmentArgs.fromBundle(getArguments());
+        titre.setText("Info in chapter "+(args.getCountryId()+1));
+
+        view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(DetailFragment.this)
+                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+            }
+        });
+    }
+}
